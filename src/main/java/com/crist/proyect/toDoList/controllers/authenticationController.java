9e1f57c9b1f4d8baa3594e4c46fc1477b696a770
cristhian <m11mario11m@gmail.com>
2023-04-20 08:31:08 -0500

    i forgot the controller xd , add endPoints /login , /register

--- src/main/java/com/crist/proyect/toDoList/controllers/authenticationController.java
+++ src/main/java/com/crist/proyect/toDoList/controllers/authenticationController.java
@@ -1,12 +1,28 @@
 package com.crist.proyect.toDoList.controllers;
 
+import com.crist.proyect.toDoList.models.auth.RegisterRequest;
+import com.crist.proyect.toDoList.models.auth.RequestAuth;
+import com.crist.proyect.toDoList.models.auth.ResponseAuth;
+import com.crist.proyect.toDoList.services.IAuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth/")
 public class authenticationController {
 
+    @Autowired
+    private IAuthenticationService serviceAuth;
+
+    @PostMapping("register")
+    public ResponseEntity<ResponseAuth> register(@RequestBody RegisterRequest payload){
+        return ResponseEntity.ok(serviceAuth.register(payload));
+    }
+
+    @PostMapping("login")
+    public ResponseEntity<ResponseAuth> login(@RequestBody RequestAuth payload){
+        return ResponseEntity.ok(serviceAuth.authentication(payload));
+    }
 
 }
